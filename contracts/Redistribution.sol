d18f30a85019d6654a8cb31aa1dcf050ebe0f9bd
李婷婷 <tina1998612@users.noreply.github.com>
2018-11-16 18:16:29 +0800

    workable code :D

--- contracts/Redistribution.sol
+++ contracts/Redistribution.sol
@@ -6,16 +6,20 @@ pragma solidity ^0.4.24;
 /// @dev TODO:
 ///           "token" to be determined; USDT considered;
 ///           owner not yet integrated; rename events and mappings to distinguish
-contract Redistribution {
 
-  enum Role {Customer, Merchant, Owner}
+import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
+contract Redistribution is ERC20 {
+
+  enum Role {None, Customer, Merchant, Owner}
 
   mapping (address => Role) public addressToRole;
   mapping (bytes32 => bool) public paymentSettled;
   mapping (bytes32 => bool) public paymentInitiated;
   mapping (bytes32 => address) public paymentHashToMerchant;
 
-  uint256 nonce;
+  uint256 public nonce;
+  address public owner;
 
   event PaymentInitiated(
     address customer,
@@ -44,10 +48,15 @@ contract Redistribution {
   }
 
   modifier paymentHasBeenInitiated(bytes32 paymentHash) {
-    require(PaymentInitiated[paymentHash] == true);
+    require(paymentInitiated[paymentHash] == true);
     _;
   }
-
+  
+  constructor() public {
+      owner = msg.sender;
+      // the token issuer has 1000 tokens initially
+      _mint(owner, 1000);
+  }
 
   function registerAsCustomer() public {
     require(addressToRole[msg.sender] != Role.Customer);
@@ -72,14 +81,14 @@ contract Redistribution {
     public
   {
     // the actual token goes to the owner
-    token.transfer(owner, _value);
+    transfer(owner, _value);
 
     // increase nonce
     nonce = nonce.add(1);
 
     // generate a hash as the unique identifier for this payment
     // @notice may NOT be unique. Can be somehow forgeable!!!
-    bytes32 _paymentHash = keccak256(abi.encoded(msg.sender, _merchant, _value, now, nonce));
+    bytes32 _paymentHash = keccak256(abi.encodePacked(msg.sender, _merchant, _value, now, nonce));
 
     // all the requirements
     require(paymentInitiated[_paymentHash] == false);
